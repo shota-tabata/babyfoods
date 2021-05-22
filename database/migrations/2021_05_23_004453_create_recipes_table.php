9b6d2fc68ee44e1d75b38383f2f3c90d823167ef
shota.tabata <yotubato4289@gmail.com>
2021-05-22 16:03:29 +0000

    create_recipes_table

---
+++ database/migrations/2021_05_23_004453_create_recipes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('child_age');
+            $table->string('description');
+            $table->string('material');
+            $table->string('how_to_make');
+            $table->string('recipe_image');
+            $table->timestamps();
+            
+            //外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
