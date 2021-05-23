b5f3708a8812d3777932d2cc061fb2d1189dda00
shota.tabata <yotubato4289@gmail.com>
2021-05-23 17:04:24 +0000

    投稿Route作成

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('recipes', 'RecipesController', ['only' => ['store', 'destroy']]);
+});
